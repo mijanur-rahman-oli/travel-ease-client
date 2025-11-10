ab0296e3bb934173f4f71fc435991bf38df1897b
mijanur-rahman-oli <sheikhmijanurrahmanoli@gmail.com>
2025-11-10 14:13:24 +0600

    optimizing routes

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -5,8 +5,6 @@ import Register from "../Pages/Auth/Register";
 import Home from "../Pages/Home/Home";
 import AllVehicles from "../Pages/AllVehicles/AllVehicles";
 
-
-
 export const router = createBrowserRouter([
   {
     path: "/",
@@ -15,13 +13,20 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home />,
-        loader: () => fetch('http://localhost:3000/latest-vehicles')
-      },
+        loader: async () => {
+          const response = await fetch('http://localhost:3000/latest-vehicles');
+          if (!response.ok) throw new Error('Failed to fetch');
+          return response.json();
+        }
+      }, 
       {
         path: "/all-vehicles",
         element: <AllVehicles />,
-        loader: () => fetch('http://localhost:3000/vehicles')
-
+        loader: async () => {
+          const response = await fetch('http://localhost:3000/vehicles');
+          if (!response.ok) throw new Error('Failed to fetch');
+          return response.json();
+        }
       },
       {
         path: "/auth/login",
